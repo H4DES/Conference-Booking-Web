3d01e9dee03248d889f673c339cdbcf81fe30797
JedAngelo <tanjedangelo@gmail.com>
2024-10-14 11:57:55 +0800

    calendar api event

--- src/services/booknig-service/booking.service.ts
+++ src/services/booknig-service/booking.service.ts
@@ -3,17 +3,38 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IApiResponse } from '../../model/api-response';
 import { Booking } from '../../model/booking';
+import { Conference } from '../../model/conference';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookingService {
 
-  private apiUrl = "";
+  private apiUrl = "https://localhost:44307/api/";
   constructor(private http: HttpClient) { }
 
-  public onGetBookingByConferenceId(): Observable<IApiResponse<Booking[]>> {
-    return this.http.get<IApiResponse<Booking[]>>(this.apiUrl +  '')
+
+  // Get calls
+  public onGetBookingByConferenceId(ID: number): Observable<IApiResponse<Booking[]>> {
+    return this.http.get<IApiResponse<Booking[]>>(this.apiUrl +  'Booking/GetBookingByConferenceID/' + ID)
+  }
+
+  public onGetBookingByBookingId(ID: number): Observable<IApiResponse<Booking>> {
+    return this.http.get<IApiResponse<Booking>>(this.apiUrl + 'Booking/GetBookingByBookingID/' + ID)
+  }
+
+  public onGetAllBooking(): Observable<IApiResponse<Booking[]>> {
+    return this.http.get<IApiResponse<Booking[]>>(this.apiUrl + 'Booking/GetAllBooking')
+  }
+
+  // Post calls
+  public onAddOrUpdateBooking(data: Booking): Observable<IApiResponse<Booking>> {
+    return this.http.post<IApiResponse<Booking>>(this.apiUrl + 'Booking/AddOrUpdateBooking', data);
+  }
+
+  // Delete calls
+  public onBookingDelete(ID: number): Observable<IApiResponse<Booking>> {
+    return this.http.delete<IApiResponse<Booking>>(this.apiUrl + 'Booking/DeleteBooking/' + ID)
   }
 
 }
