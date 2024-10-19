7bc7a71f2cef3272d322fa95192f783c7fb0cf96
JedAngelo <tanjedangelo@gmail.com>
2024-10-19 21:45:13 +0800

    updated api url

--- src/services/booknig-service/booking.service.ts
+++ src/services/booknig-service/booking.service.ts
@@ -10,7 +10,7 @@ import { Conference } from '../../model/conference';
 })
 export class BookingService {
 
-  private apiUrl = "https://100.90.130.38:7026/api/";
+  private apiUrl = "https://conferencebooking.local/api/";
   constructor(private http: HttpClient) { }
 
 
