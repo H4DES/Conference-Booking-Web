8da6ad6df65c2f189f74ad3bd8b95b81bcefa2d8
H4DES <bernardporlas93@gmail.com>
2024-10-31 10:45:49 +0800

    Added the AppConfigService

--- src/services/auth-service/register.service.ts
+++ src/services/auth-service/register.service.ts
@@ -1,19 +1,22 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Register } from '../../model/register';
 import { Observable } from 'rxjs';
 import { IApiResponse } from '../../model/api-response';
+import { AppComponent } from '../../app/app.component';
+import { AppConfigService } from '../../app/AppConfigService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  private apiUrl = "https://100.90.130.38:2401/api/";
+  // private apiUrl = "https://100.90.130.38:2401/api/";
+  // private apiUrl = "https://100.100.252.81:2501/api/";
   // private apiUrl = "https://localhost:7026/api/";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private config: AppConfigService) { }
 
   public onRegisterUser(data: Register): Observable<IApiResponse<Register>> {
-    return this.http.post<IApiResponse<Register>>(this.apiUrl + 'UserAuth/RegisterUser', data);
+    return this.http.post<IApiResponse<Register>>(this.config.apiUrl + 'UserAuth/RegisterUser', data);
   }
 }
--- src/services/conference-service/conference.service.ts
+++ src/services/conference-service/conference.service.ts
@@ -1,31 +1,32 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Conference } from '../../model/conference';
 import { IApiResponse } from '../../model/api-response';
 import { Observable } from 'rxjs';
+import { AppComponent } from '../../app/app.component';
+import { AppConfigService } from '../../app/AppConfigService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConferenceService {
 
-  private apiUrl = "https://100.90.130.38:2401/api/";
   // private apiUrl = "https://100.90.130.38:7026/api/";
   // private apiUrl = "https://localhost:7026/api/";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private config: AppConfigService) { }
 
   // Get calls
   public onGetAllConference(): Observable<IApiResponse<Conference[]>> {
-    return this.http.get<IApiResponse<Conference[]>>(this.apiUrl + 'Conference/GetAllConference')
+    return this.http.get<IApiResponse<Conference[]>>(this.config.apiUrl + 'Conference/GetAllConference')
   }
 
   // Post calls
   public onAddOrUpdateConference(data: Conference): Observable<IApiResponse<Conference>> {
-    return this.http.post<IApiResponse<Conference>>(this.apiUrl + 'Conference/AddOrUpdateConference', data)
+    return this.http.post<IApiResponse<Conference>>(this.config.apiUrl + 'Conference/AddOrUpdateConference', data)
   }
 
   // Delete calls
   public onConferenceDelete(ID: number): Observable<IApiResponse<Conference>> {
-    return this.http.delete<IApiResponse<Conference>>(this.apiUrl + 'Conference/DeleteConference/' + ID)
+    return this.http.delete<IApiResponse<Conference>>(this.config.apiUrl + 'Conference/DeleteConference/' + ID)
   }
 }
