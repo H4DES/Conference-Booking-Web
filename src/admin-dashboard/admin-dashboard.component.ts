1794e46dd24f5597ef7d8a26b244369f3c5257cf
H4DES <bernardporlas93@gmail.com>
2024-10-31 08:41:55 +0800

    Added admin-dashboard component

--- src/admin-dashboard/admin-dashboard.component.ts
+++ src/admin-dashboard/admin-dashboard.component.ts
@@ -1,20 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClassModule } from 'primeng/styleclass';
+import { Sidebar } from 'primeng/sidebar';
+import { DividerModule } from 'primeng/divider';
+import { CardModule } from 'primeng/card';
+
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, SidebarModule, ButtonModule, RippleModule, 
+            AvatarModule, StyleClassModule, DividerModule, CardModule],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
 
-  status = false;
-  addToggle()
-{
-  this.status = !this.status;       
-}
+    closeCallback(e: any): void {
+        this.sidebarRef.close(e);
+    }
+  sidebarVisible: boolean = false;
 
 }
