a67a1aaf646a85d54807476d1e395b4fb7c7ac93
JedAngelo <tanjedangelo@gmail.com>
2024-11-13 09:07:45 +0800

    fixed model

--- src/app/AppConfigService.ts
+++ src/app/AppConfigService.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AppConfigService {
-  public readonly apiUrl = "https://100.90.130.38:2401/api/";//2501
-  // public readonly apiUrl = "https://100.100.252.81:2501/api/";//2501
+  // public readonly apiUrl = "https://100.90.130.38:2401/api/";
+  public readonly apiUrl = "https://100.100.252.81:2501/api/";
 
 }
